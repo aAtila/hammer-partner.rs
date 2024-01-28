d268090db7b455664def7b9a055d658e0a56d1fe
Atila Alacan <alacanatila@gmail.com>
2024-01-28 05:42:22 +0100

    add product list to products page

--- app/routes/admin.proizvodi._index.tsx
+++ app/routes/admin.proizvodi._index.tsx
@@ -1,9 +1,24 @@
 import { Button } from '~/components/ui/button';
 import PageTitle from './admin/components/page-title';
 import { PlusCircleIcon } from 'lucide-react';
-import { Link } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
+import { prisma } from '~/db/db.server';
+
+export const loader = async () => {
+	const products = await prisma.product.findMany({
+		select: {
+			id: true,
+			name: true,
+			manufacturer: true,
+			price: true,
+		},
+	});
+
+	return { products };
+};
 
 export default function ProductPage() {
+	const { products } = useLoaderData<typeof loader>();
 	return (
 		<>
 			<PageTitle title="Proizvodi" description="Lista svih proizvoda">
@@ -14,6 +29,15 @@ export default function ProductPage() {
 					</Link>
 				</Button>
 			</PageTitle>
+			<section className="p-8">
+				<ul>
+					{products.map(({ id, name, price, manufacturer }) => (
+						<Link key={id} to={`/proizvodi/${id}`} prefetch="intent">
+							<li className="underline decoration-transparent decoration-1 underline-offset-4 transition-all after:content-['_↗'] hover:decoration-foreground">{`${name} | ${manufacturer} | ${price} RSD`}</li>
+						</Link>
+					))}
+				</ul>
+			</section>
 		</>
 	);
 }
