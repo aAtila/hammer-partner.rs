f7420315aa50bda28c7301e858667e27bef09b7e
Atila Alacan <alacanatila@gmail.com>
2024-01-31 08:33:14 +0100

    restyling product page

--- app/routes/admin.proizvodi._index.tsx
+++ app/routes/admin.proizvodi._index.tsx
@@ -4,15 +4,21 @@ import { PlusCircleIcon } from 'lucide-react';
 import { Link, useLoaderData } from '@remix-run/react';
 import { prisma } from '~/db/db.server';
 import CreateButton from './admin/components/create-button';
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '~/components/ui/table';
+import { formatDate } from '~/utils/date';
+import { toCurrency } from '~/utils/number';
 
 export const loader = async () => {
 	const products = await prisma.product.findMany({
-		select: {
-			id: true,
-			name: true,
-			manufacturer: true,
-			price: true,
-		},
+		include: { category: { select: { name: true } } },
 	});
 
 	return { products };
@@ -22,17 +28,41 @@ export default function ProductPage() {
 	const { products } = useLoaderData<typeof loader>();
 	return (
 		<>
-			<PageTitle title="Proizvodi" description="Lista svih proizvoda">
-				<CreateButton>Dodaj proizvod</CreateButton>
-			</PageTitle>
+			<PageTitle title="Proizvodi" description="Lista svih proizvoda" />
 			<section className="p-8">
-				<ul>
-					{products.map(({ id, name, price, manufacturer }) => (
-						<Link key={id} to={`/admin/proizvodi/${id}`} prefetch="intent">
-							<li className="underline decoration-transparent decoration-1 underline-offset-4 transition-all after:content-['_↗'] hover:decoration-foreground">{`${name} | ${manufacturer} | ${price} RSD`}</li>
-						</Link>
-					))}
-				</ul>
+				<div className="text-right">
+					<CreateButton>Dodaj proizvod</CreateButton>
+				</div>
+				<div className="mt-3 rounded border">
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">SKU</TableHead>
+								<TableHead>Naziv</TableHead>
+								<TableHead>Brend</TableHead>
+								<TableHead>#</TableHead>
+								<TableHead>Cena</TableHead>
+								<TableHead>Dostupnost</TableHead>
+								<TableHead className="text-right">Kreiran</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{products.map((product) => (
+								<TableRow key={product.id}>
+									<TableCell className="font-medium">{product.sku}</TableCell>
+									<TableCell>{product.name}</TableCell>
+									<TableCell>{product.category.name}</TableCell>
+									<TableCell>{product.quantity}</TableCell>
+									<TableCell>{toCurrency(product.price)}</TableCell>
+									<TableCell>{product.availability}</TableCell>
+									<TableCell className="text-right">
+										{formatDate(product.createdAt)}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
 			</section>
 		</>
 	);
