061e38788b8c42aa9ce9f036c1538deca4fd39a0
Atila Alacan <alacanatila@gmail.com>
2024-01-31 08:11:04 +0100

    categories page with optimistic ui

--- app/routes/admin.kategorije.tsx
+++ app/routes/admin.kategorije.tsx
@@ -7,11 +7,33 @@ import type { ActionFunctionArgs } from '@remix-run/node';
 import { categoryFormSchema } from './admin/schemas';
 import { prisma } from '~/db/db.server';
 import FormErrors from './admin/components/form-errors';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import type { Category } from '@prisma/client';
+import type { HttpResponse } from '~/services/http';
+import { HttpResponses } from '~/services/http';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '~/components/ui/table';
+import { PencilIcon, Trash2Icon } from 'lucide-react';
+import { flushSync } from 'react-dom';
+
+const Intent = {
+	Create: 'create',
+	Update: 'update',
+	Delete: 'delete',
+} as const;
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	const formData = Object.fromEntries(await request.formData());
-	const parsedData = categoryFormSchema.safeParse(formData);
+	const formData = await request.formData();
+	const categoryId = String(formData.get('id'));
+	const data = Object.fromEntries(formData);
+
+	const parsedData = categoryFormSchema.safeParse(data);
 
 	if (!parsedData.success) {
 		return json(
@@ -24,21 +46,48 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 	const categoryData = parsedData.data;
 
-	const category = await prisma.category.create({
-		data: categoryData,
-	});
+	let category: Category;
+	let statusCode: HttpResponse = HttpResponses.Ok;
 
-	return json({ category });
+	switch (data.intent) {
+		case Intent.Create:
+			category = await prisma.category.create({
+				data: categoryData,
+			});
+			statusCode = HttpResponses.Created;
+			break;
+		case Intent.Update:
+			category = await prisma.category.update({
+				where: { id: categoryId },
+				data: { name: categoryData.name, slug: categoryData.slug },
+			});
+			break;
+		case Intent.Delete:
+			category = await prisma.category.update({
+				where: { id: categoryId },
+				data: { deletedAt: new Date() },
+			});
+			break;
+		default:
+			return json('Something went wrong', { status: HttpResponses.BadRequest });
+	}
+
+	return json({ category }, { status: statusCode });
 };
 
 export const loader = async () => {
 	const categories = await prisma.category.findMany({
-		include: {
+		select: {
+			id: true,
+			name: true,
 			products: {
 				select: {
 					id: true,
 				},
 			},
+		},
+		where: {
+			deletedAt: null,
 		},
 	});
 
@@ -47,6 +96,9 @@ export const loader = async () => {
 
 export default function CategoriesPage() {
 	const { categories } = useLoaderData<typeof loader>();
+	const fetcher = useFetcher();
+	const [edit, setEdit] = useState<string | null>(null);
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	return (
 		<>
@@ -54,22 +106,76 @@ export default function CategoriesPage() {
 				title="Kategorije"
 				description="Lista svih dostupnih kategorija"
 			/>
-			<section className="mt-8 space-y-8 text-muted-foreground">
-				<CreateCategoryForm />
-				<ul>
-					{categories.map(({ id, name, slug, products }) => (
-						<li key={id}>
-							{name} ({products && products.length ? products.length : 0})
-						</li>
-					))}
-				</ul>
+			<section className="mt-8 max-w-lg space-y-8 text-muted-foreground">
+				<div className="rounded border">
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="min-w-[250px]">Naziv</TableHead>
+								<TableHead className="text-center">Proizvodi</TableHead>
+								<TableHead className="text-right">Akcije</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{categories.map(({ id, name, products }) => (
+								<TableRow key={id}>
+									<TableCell className="font-medium">
+										<EditableText
+											fieldName="name"
+											id={id}
+											value={name}
+											isEditing={edit === id}
+											onClose={() => setEdit(null)}
+										/>
+									</TableCell>
+									<TableCell className="text-center">
+										{products.length}
+									</TableCell>
+									<TableCell className="text-right">
+										<div className="flex justify-end gap-4">
+											<PencilIcon
+												type="submit"
+												className="size-4 cursor-pointer transition-colors hover:text-sky-600"
+												onClick={() => {
+													flushSync(() => {
+														setEdit(id);
+													});
+													inputRef.current?.focus();
+												}}
+											/>
+											<fetcher.Form method="post">
+												<input type="text" name="id" defaultValue={id} hidden />
+												<input
+													type="text"
+													name="name"
+													defaultValue={name}
+													hidden
+												/>
+												<button
+													type="submit"
+													name="intent"
+													value={Intent.Delete}
+												>
+													<Trash2Icon className="size-4 transition-colors hover:text-destructive" />
+												</button>
+											</fetcher.Form>
+										</div>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+				<div className="bg-white">
+					<CreateCategoryForm />
+				</div>
 			</section>
 		</>
 	);
 }
 
 const CreateCategoryForm = () => {
-	const createFetcher = useFetcher({ key: 'createCategory' });
+	const createFetcher = useFetcher();
 	const formRef = useRef<HTMLFormElement>(null);
 
 	useEffect(() => {
@@ -82,20 +188,80 @@ const CreateCategoryForm = () => {
 		<div>
 			<FormErrors errors={createFetcher.data?.errors} />
 			<createFetcher.Form
-				className="mx-auto mt-8 grid max-w-lg grid-cols-[auto_1fr] items-center gap-3"
+				className="mt-16 grid gap-4"
 				method="post"
 				ref={formRef}
 			>
-				<Label htmlFor="name">Naziv kategorije:</Label>
-				<Input type="text" name="name" />
-				<Label htmlFor="slug">Url slug:</Label>
-				<Input type="text" name="slug" />
+				<Label htmlFor="name" className="text-lg text-foreground">
+					Dodaj novu kategoriju
+				</Label>
+				<Input type="text" name="name" placeholder="Naziv nove kategorije" />
 				<div className="col-span-full text-right">
-					<Button type="submit" disabled={createFetcher.state === 'submitting'}>
+					<Button
+						type="submit"
+						disabled={createFetcher.state === 'submitting'}
+						value={Intent.Create}
+						name="intent"
+					>
 						Dodaj
 					</Button>
 				</div>
 			</createFetcher.Form>
 		</div>
+	);
+};
+
+const EditableText = ({
+	fieldName,
+	value,
+	id,
+	isEditing,
+	onClose,
+}: {
+	fieldName: string;
+	value: string;
+	id: string;
+	isEditing: boolean;
+	onClose: () => void;
+}) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const fetcher = useFetcher();
+
+	useEffect(() => {
+		if (isEditing) {
+			inputRef.current?.focus();
+		}
+	}, [isEditing]);
+
+	if (fetcher.formData?.get('name')) {
+		value = fetcher.formData.get('name') as string;
+	}
+
+	return isEditing ? (
+		<fetcher.Form method="post">
+			<input type="text" name="intent" defaultValue={Intent.Update} hidden />
+			<input type="text" name="id" defaultValue={id} hidden />
+			<Input
+				type="text"
+				name={fieldName}
+				defaultValue={value}
+				className="-my-2 h-8"
+				ref={inputRef}
+				onBlur={onClose}
+				onKeyDown={(event) => {
+					switch (event.key) {
+						case 'Escape':
+							onClose();
+							break;
+						case 'Enter':
+							fetcher.submit(event.currentTarget.form);
+							onClose();
+							break;
+					}
+				}}
+			/>
+		</fetcher.Form>
+	) : (
+		value
 	);
 };
