3a48b20543ff59bd8fa5a462f8de59641baa88c3
Atila Alacan <alacanatila@gmail.com>
2024-01-31 23:10:22 +0100

    soft delete product

--- app/routes/admin.proizvodi.tsx
+++ app/routes/admin.proizvodi.tsx
@@ -1,5 +1,5 @@
 import PageTitle from './admin/components/page-title';
-import { Outlet, useLoaderData } from '@remix-run/react';
+import { Link, Outlet, useFetcher, useLoaderData } from '@remix-run/react';
 import { prisma } from '~/db/db.server';
 import CreateButton from './admin/components/create-button';
 import {
@@ -12,9 +12,28 @@ import {
 } from '~/components/ui/table';
 import { formatDate } from '~/utils/date';
 import { toCurrency } from '~/utils/number';
+import { EyeIcon, PencilIcon, Trash2Icon } from 'lucide-react';
+import { Intent } from './admin.kategorije';
+import type { ActionFunctionArgs } from '@remix-run/node';
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const formData = await request.formData();
+	const productId = String(formData.get('id'));
+	const intent = String(formData.get('intent'));
+
+	if (intent === Intent.Delete) {
+		await prisma.product.update({
+			where: { id: productId },
+			data: { deletedAt: new Date() },
+		});
+	}
+
+	return { status: 'deleted' };
+};
 
 export const loader = async () => {
 	const products = await prisma.product.findMany({
+		where: { deletedAt: null },
 		include: { category: { select: { name: true } } },
 	});
 
@@ -23,6 +42,7 @@ export const loader = async () => {
 
 export default function ProductPage() {
 	const { products } = useLoaderData<typeof loader>();
+	const fetcher = useFetcher();
 
 	return (
 		<>
@@ -37,11 +57,12 @@ export default function ProductPage() {
 							<TableRow>
 								<TableHead className="w-[100px]">SKU</TableHead>
 								<TableHead>Naziv</TableHead>
-								<TableHead>Brend</TableHead>
-								<TableHead>#</TableHead>
+								<TableHead>Kategorija</TableHead>
 								<TableHead>Cena</TableHead>
 								<TableHead>Dostupnost</TableHead>
-								<TableHead className="text-right">Kreiran</TableHead>
+								<TableHead>#</TableHead>
+								<TableHead>Kreiran</TableHead>
+								<TableHead className="text-right"></TableHead>
 							</TableRow>
 						</TableHeader>
 						<TableBody>
@@ -53,8 +74,25 @@ export default function ProductPage() {
 									<TableCell>{product.quantity}</TableCell>
 									<TableCell>{toCurrency(product.price)}</TableCell>
 									<TableCell>{product.availability}</TableCell>
+									<TableCell>{formatDate(product.createdAt)}</TableCell>
 									<TableCell className="text-right">
-										{formatDate(product.createdAt)}
+										<div className="flex gap-2">
+											<fetcher.Form method="post">
+												<input
+													type="text"
+													name="id"
+													defaultValue={product.id}
+													hidden
+												/>
+												<button
+													type="submit"
+													name="intent"
+													value={Intent.Delete}
+												>
+													<Trash2Icon className="size-4 transition-colors hover:text-destructive" />
+												</button>
+											</fetcher.Form>
+										</div>
 									</TableCell>
 								</TableRow>
 							))}
