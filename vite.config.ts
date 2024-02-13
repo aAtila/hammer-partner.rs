c4b54f2f97c6f127a35478468ad88042797d445e
Atila Alacan <alacanatila@gmail.com>
2024-02-13 05:12:41 +0100

    install the global polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { unstable_vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { remixDevTools } from 'remix-development-tools/vite';
+import { installGlobals } from '@remix-run/node';
+
+installGlobals();
 
 export default defineConfig({
 	server: {
