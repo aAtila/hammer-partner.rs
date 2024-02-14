1aa0bf67a9825755477f5dae9dcb1125b540cc7b
Atila Alacan <alacanatila@gmail.com>
2024-02-14 18:44:11 +0100

    create folder action

--- app/services/publitio.server.ts
+++ app/services/publitio.server.ts
@@ -2,11 +2,75 @@ import crypto from 'crypto';
 
 const PUBLITIO_API_KEY = process.env.PUBLITIO_API_KEY ?? '';
 const PUBLITIO_API_SECRET = process.env.PUBLITIO_API_SECRET ?? '';
+const PUBLITIO_PARENT_ID = 'AelL3Jjb';
 
 export const uploadImage = async (file: File) => {
 	const formData = new FormData();
 	formData.append('file', file);
 
+	const authParams = getAuthSignature();
+	const url = `https://api.publit.io/v1/files/create?${authParams}`;
+
+	fetch(url, {
+		method: 'POST',
+		body: formData,
+	})
+		.then((response) => response.json())
+		.then((json) => console.log({ json }))
+		.catch((error) => console.log({ error }));
+};
+
+type PublitioFolderResponse = {
+	success: boolean;
+	code: number;
+	parent_id: null | string;
+	parent_path: string;
+	folders_count: number;
+	folders: Array<{
+		id: string;
+		name: string;
+		path: string;
+		parent_id: null | string;
+		created_at: string;
+		updated_at: string;
+	}>;
+};
+
+export const listFolders = async (): Promise<PublitioFolderResponse> => {
+	const authParams = getAuthSignature();
+	const url = `https://api.publit.io/v1/folders/list?parent_id=${PUBLITIO_PARENT_ID}&${authParams}`;
+
+	return fetch(url)
+		.then((response) => response.json())
+		.catch((error) => console.log({ error }));
+};
+
+type CreateFolderResponse = {
+	success: boolean;
+	code: number;
+	message: string;
+	id: string;
+	name: string;
+	path: string;
+	parent_id: string | null;
+	created_at: string;
+	updated_at: string;
+};
+
+export const createFolder = async (
+	name: string,
+): Promise<CreateFolderResponse> => {
+	const authParams = getAuthSignature();
+	const url = `https://api.publit.io/v1/folders/create?parent_id=${PUBLITIO_PARENT_ID}&name=${name}&${authParams}`;
+
+	return fetch(url, {
+		method: 'POST',
+	})
+		.then((response) => response.json())
+		.catch((error) => console.log({ error }));
+};
+
+const getAuthSignature = (): string => {
 	const api_timestamp = Math.floor(Date.now() / 1000);
 	const api_nonce = Math.floor(Math.random() * 99999999)
 		.toString()
@@ -16,13 +80,6 @@ export const uploadImage = async (file: File) => {
 		.createHash('sha1')
 		.update(signatureString)
 		.digest('hex');
-	const url = `https://api.publit.io/v1/files/create?api_key=${PUBLITIO_API_KEY}&api_nonce=${api_nonce}&api_signature=${api_signature}&api_timestamp=${api_timestamp}`;
 
-	fetch(url, {
-		method: 'POST',
-		body: formData,
-	})
-		.then((response) => response.json())
-		.then((json) => console.log({ json }))
-		.catch((error) => console.log({ error }));
+	return `api_signature=${api_signature}&api_key=${PUBLITIO_API_KEY}&api_nonce=${api_nonce}&api_timestamp=${api_timestamp}`;
 };
